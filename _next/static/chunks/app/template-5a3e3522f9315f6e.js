b5e107c5aadd0c9cbd0a9b32ec48dc0427de2baa
ifirmawan <hellofirmawan@gmail.com>
2024-12-18 16:13:54 +0700

    Updates

---
+++ _next/static/chunks/app/template-5a3e3522f9315f6e.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[489],{6386:function(e,n,s){Promise.resolve().then(s.t.bind(s,8173,23)),Promise.resolve().then(s.t.bind(s,231,23)),Promise.resolve().then(s.t.bind(s,4262,23)),Promise.resolve().then(s.t.bind(s,3810,23))}},function(e){e.O(0,[408,225,971,23,744],function(){return e(e.s=6386)}),_N_E=e.O()}]);
